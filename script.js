f6c1cc8adc4b1b49bab123ee01c26485877e73eb
Robert Anthony <rwanthony3@gmail.com>
2022-02-21 23:15:31 -0800

    button functionality

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-//Dice 1
-const firstRandomNum = Math.floor(Math.random() *6) +1;
+function rollTheDice() {
+    //Dice 1
+    const firstRandomNum = Math.floor(Math.random() *6) +1;
+    const firstDiceImage = "Assets/Dice Faces/" + firstRandomNum +  ".svg";
+    document.querySelector("#dice1").setAttribute("src", firstDiceImage);
 
-const firstDiceImage = "Assets/Dice Faces/" + firstRandomNum +  ".svg";
+    //Dice 2
+    const secondRandomNum = Math.floor(Math.random() *6) +1;
+    const secondDiceImage = "Assets/Dice Faces/" + secondRandomNum +  ".svg";
+    document.querySelector("#dice2").setAttribute("src", secondDiceImage);
 
-document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
+    //Dice3
+    const thirdRandomNum = Math.floor(Math.random() *6) +1;
+    const thirdDiceImage = "Assets/Dice Faces/" + thirdRandomNum +  ".svg";
+    document.querySelector("#dice3").setAttribute("src", thirdDiceImage);
+}
 
-//Dice 2
-const secondRandomNum = Math.floor(Math.random() *6) +1;
-
-const secondDiceImage = "Assets/Dice Faces/" + secondRandomNum +  ".svg";
-
-document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
-
-//Dice3
-const thirdRandomNum = Math.floor(Math.random() *6) +1;
-
-const thirdDiceImage = "Assets/Dice Faces/" + thirdRandomNum +  ".svg";
-
-document.querySelectorAll("img")[2].setAttribute("src", thirdDiceImage);
-
-const cardRandomNum = Math.floor(Math.random() * 52) +1;
-
-const cardImage = "Assets/New Cards/" + cardRandomNum +  ".svg";
-
-document.querySelectorAll("img")[3].setAttribute("src", cardImage);
+function newCard() {
+    //Card
+    const cardRandomNum = Math.floor(Math.random() * 52) +1;
+    const cardImage = "Assets/New Cards/" + cardRandomNum +  ".svg";
+    document.querySelector("#openCard").setAttribute("src", cardImage);
+}
