43eb73a9525a3bb063977612df38a2b30991f7a0
Robert Anthony <rwanthony3@gmail.com>
2022-02-21 23:18:28 -0800

    new load changes

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//Dice 1
+const firstRandomNum = Math.floor(Math.random() *6) +1;
+const firstDiceImage = "Assets/Dice Faces/" + firstRandomNum +  ".svg";
+document.querySelector("#dice1").setAttribute("src", firstDiceImage);
+
+//Dice 2
+const secondRandomNum = Math.floor(Math.random() *6) +1;
+const secondDiceImage = "Assets/Dice Faces/" + secondRandomNum +  ".svg";
+document.querySelector("#dice2").setAttribute("src", secondDiceImage);
+
+//Dice3
+const thirdRandomNum = Math.floor(Math.random() *6) +1;
+const thirdDiceImage = "Assets/Dice Faces/" + thirdRandomNum +  ".svg";
+document.querySelector("#dice3").setAttribute("src", thirdDiceImage);
+
 function rollTheDice() {
     //Dice 1
     const firstRandomNum = Math.floor(Math.random() *6) +1;
