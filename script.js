00defe13a054d23a68f4e5f1a26c32d63f544220
Robert Anthony <rwanthony3@gmail.com>
2022-02-21 19:58:05 -0800

    3 Dice Roll

---
+++ script.js
@@ -0,0 +1,21 @@
+//Dice 1
+//generate random number between 1 and 6
+const firstRandomNum = Math.floor(Math.random() *6) +1;
+
+const firstDiceImage = "Assets/Dice Faces/" + firstRandomNum +  ".svg";
+
+document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
+
+//Dice 2
+const secondRandomNum = Math.floor(Math.random() *6) +1;
+
+const secondDiceImage = "Assets/Dice Faces/" + secondRandomNum +  ".svg";
+
+document.querySelectorAll("img")[1].setAttribute("src", secondDiceImage);
+
+//Dice3
+const thirdRandomNum = Math.floor(Math.random() *6) +1;
+
+const thirdDiceImage = "Assets/Dice Faces/" + thirdRandomNum +  ".svg";
+
+document.querySelectorAll("img")[2].setAttribute("src", thirdDiceImage);
